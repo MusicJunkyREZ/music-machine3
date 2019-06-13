69d992ccc5035ee78cd56657dc1e4ddd09e4187c
MusicJunkyREZ <reidzuckerman@gmail.com>
2019-06-13 15:31:09 -0600

    added build and took out of gitignore

---
+++ client/build/precache-manifest.2a92fc17888dae77183130ee838c9112.js
@@ -0,0 +1,34 @@
+window.self.__precacheManifest = [
+  {
+    "revision": "d6b6b2c0ac3c8e35c199",
+    "url": "/static/css/main.261fcbdf.chunk.css"
+  },
+  {
+    "revision": "d6b6b2c0ac3c8e35c199",
+    "url": "/static/js/main.d6b6b2c0.chunk.js"
+  },
+  {
+    "revision": "95bc649363730b2483f0",
+    "url": "/static/css/1.b6843a69.chunk.css"
+  },
+  {
+    "revision": "95bc649363730b2483f0",
+    "url": "/static/js/1.95bc6493.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "5fc3539fa2eaec342eb3011184f6267c",
+    "url": "/index.html"
+  }
+];
